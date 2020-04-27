5ec291f08324c4accdaf5c257f4967a6352c326e
Will <willmorgusapps@gmail.com>
2020-04-27 16:32:37 -0400

    final cleaning for top, million

---
+++ scripts/final_filter.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import pandas as pd
+import requests
+
+import csv
+
+def main():
+ 
+  problems = []
+  with open("./all_songs/problems.txt") as file_in:
+    for uri in file_in:
+      problems.append(uri)
+  print(len(problems))
+
+  millionDF = pd.read_csv('/Users/willmorgus/Desktop/550_proj/scripts/new_playlists/millionplaylists.csv')
+  print(millionDF.shape[0])
+  print(millionDF[millionDF['sid'].isin(problems)].index)
+  millionDF = millionDF.drop(millionDF[millionDF['sid'].isin(problems)].index)
+  print(millionDF.shape[0])
+  topDF = pd.read_csv('/Users/willmorgus/Desktop/550_proj/scripts/top_songs/topsongs.csv')
+  print(topDF.shape[0])
+  print(topDF[topDF['sid'].isin(problems)].index)
+  topDF = topDF.drop(topDF[topDF['sid'].isin(problems)].index)
+  print(topDF.shape[0])
+
+
+  millionDF.to_csv('new_millionplaylists.csv', index=False, encoding = 'utf-8')
+  topDF.to_csv('new_topsongs.csv', index=False, encoding = 'utf-8')
+
+main()
