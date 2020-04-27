cdae8800d753c3b252f0d4c04ef46f5221595b66
Will <willmorgusapps@gmail.com>
2020-04-27 16:37:22 -0400

    adding owners

--- scripts/final_filter.py
+++ scripts/final_filter.py
@@ -18,6 +18,12 @@ def main():
   print(millionDF[millionDF['sid'].isin(problems)].index)
   millionDF = millionDF.drop(millionDF[millionDF['sid'].isin(problems)].index)
   print(millionDF.shape[0])
+
+  ownerDF = pd.DataFrame(columns=['pid', 'oid'])
+  ownerDF['pid'] = millionDF['pid']
+  ownerDF['oid'] = ownerDF['oid'].apply(lambda x: 'million_playlist')
+  print(ownerDF)
+
   topDF = pd.read_csv('/Users/willmorgus/Desktop/550_proj/scripts/top_songs/topsongs.csv')
   print(topDF.shape[0])
   print(topDF[topDF['sid'].isin(problems)].index)
@@ -25,7 +31,9 @@ def main():
   print(topDF.shape[0])
 
 
+
   millionDF.to_csv('new_millionplaylists.csv', index=False, encoding = 'utf-8')
   topDF.to_csv('new_topsongs.csv', index=False, encoding = 'utf-8')
+  ownerDF.to_csv('million_owners.csv', index=False, encoding = 'utf-8')
 
 main()
