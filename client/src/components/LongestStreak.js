82f646d318caea0e8722574348da8d7cbec9875a
Will <willmorgusapps@gmail.com>
2020-05-02 18:13:32 -0400

    i hate this so very much, but it works

--- client/src/components/LongestStreak.js
+++ client/src/components/LongestStreak.js
@@ -5,6 +5,7 @@ import {Button, Dropdown} from 'react-bootstrap';
 import PageNavbar from './PageNavbar';
 import TopSidRow from './TopSidRow';
 import TopSongRow from './TopSongRow';
+import CustomDropdown from './CustomDropdown';
 // import { MDBForm, MDBSelectInput, MDBSelect, MDBSelectOptions, MDBSelectOption } from "mdbreact";
 
 export default class LongestStreak extends React.Component {
@@ -15,7 +16,8 @@ export default class LongestStreak extends React.Component {
       songs: [],
       sids: [],
       sidc: [],
-      table: []
+      table: [],
+      dropdownjerns: []
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -23,6 +25,7 @@ export default class LongestStreak extends React.Component {
 
   componentDidMount() {
     // Send an HTTP request to the server.
+    var songObjs = []
     fetch("http://localhost:8081/streaksids",
     {
       method: 'GET' // The type of HTTP request.
@@ -30,14 +33,23 @@ export default class LongestStreak extends React.Component {
       console.log(data.rows)
       var result = data.rows;
       console.log(result[0]);
-      let sidDivs = result.map((songObj, i) =>{
-        if (i == 1) {
-          console.log(songObj)
-        }
-        return(<option value={songObj[0]}>{songObj[1]}</option>)
+      var dropdownjerns = []
+      var sidDivs = []
+      // let sidDivs = result.map((songObj, i) =>{
+      //   if (i == 1) {
+      //     console.log(songObj)
+      //   }
+      //   songObjs.push({name: songObj[1] + ' by ' + songObj[2], id: songObj[0]})
+      //   return(<option value={songObj[0]}>{songObj[1]}</option>)
+      // });
+      dropdownjerns = result.map((songObj, i) =>{
+        return(<Dropdown.Item onSelect={() => {
+          //handlesubmithere
+          console.log(songObj[0])
+          }}>{songObj[1] + ' by ' + songObj[2]}</Dropdown.Item>)
       });
-      console.log('siddivs\n\n\nsiddivs')
       this.setState({
+        dropdownjerns: dropdownjerns,
         sids: sidDivs
       });
     });
@@ -49,12 +61,16 @@ export default class LongestStreak extends React.Component {
 
     this.setState({
       [name]: value
-      });
-    }
+    });
+  }
 
-    handleSubmit = (event) => {
-      event.preventDefault();
+  handleDropSubmit = (id) => {
+    console.log('drop submitted: ' + id)
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event)
 		fetch("http://localhost:8081/longeststreak/", {
 		  method: 'GET' // The type of HTTP request.
 		}).then(response => response.json()).then((data) => {
@@ -72,27 +88,16 @@ export default class LongestStreak extends React.Component {
 
   render() {
     return (
-      <div className="container songtable-container">
+      <div>
         <PageNavbar active="time" apikey={this.props.apikey}/>
-        <div className="Jumbotron">
-          <div className="lander">
+        <div style={{display: ""}}>
           <div className="h5">Longest Song Streaks</div>
-            <p>How Many Consecutive Days a Song Has Stayed on the Charts</p>
+          <p>How Many Consecutive Days a Song Has Stayed on the Charts</p>
+          <div style={{maxHeight: '30%'}}>
+            <CustomDropdown dropdownjerns={this.state.dropdownjerns}/>
           </div>
         </div>
-        <form onSubmit = {this.handleSubmit} className="inputForm">
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-            </Dropdown.Toggle>
-            <Dropdown.Menu value={this.state.song} onChange={this.handleInputChange} >
-              
-            </Dropdown.Menu>
-            </Dropdown>
-            <select>
-              {this.state.sids}
-            </select>
-        </form>
+        
         
         <div className="jumbotron">
         <div className="movies-container">
@@ -105,7 +110,7 @@ export default class LongestStreak extends React.Component {
                 {this.state.songs}
               </div>
             </div>
-            </div>
+          </div>
       </div>
 
     );
