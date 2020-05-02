0544e43a59065034ff74e1a5d5cf8427bae4dcb1
lraynor <lraynor@sas.upenn.edu>
2020-05-02 00:48:02 -0400

    pulling playlists for recs

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -38,6 +38,48 @@ export default class Recommendations extends React.Component {
           console.log(err);
         });
 
+
+         
+        fetch('http://localhost:8081/spotify/getPlaylists?apikey=' + this.props.apikey).then(response => response.json()).then((data) => {
+          console.log(data);
+          data.items.forEach(item => {
+            console.log(item.owner.id);
+          });
+          //console.log(data.href);
+          
+          //find the user currently logged in 
+          var split = data.href.split('/');
+          var parsedUser; 
+          for(var i = 0; i < split.length; i++) {
+            if(split[i] == 'users') {
+              parsedUser = split[i + 1];
+            }
+          }
+          console.log('parsed user: ' + parsedUser);
+          /*
+          split.forEach(seg => {
+            console.log(seg);
+          });
+          */
+
+          //loop through all playlists and display those belonging to current user
+          var myPlaylists = [];
+          data.items.forEach(item => {
+           // console.log(item.owner.id + ' vs ' + parsedUser + ': result? ' + item.owner.id === parsedUser);
+            if(item.owner.id == parsedUser){
+              console.log(item.id + 'belongs to ' + item.owner.id);
+              myPlaylists.push(item.id); 
+            }
+          });
+          
+
+          myPlaylists.forEach(seg => {
+            console.log(seg);
+          });
+
+        });
+
+
       }
 
       render() { 
