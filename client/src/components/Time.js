13c6d5fc36e4540b90247f0017cf70baefe5021e
lhosey <hosey@seas.upenn.edu>
2020-05-02 10:51:08 -0700

    Restructured time pages, added longest streak page

--- client/src/components/Time.js
+++ client/src/components/Time.js
@@ -3,49 +3,14 @@ import '../style/Time.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
 import PageNavbar from './PageNavbar';
-import TopSongRow from './TopSongRow';
 
 export default class Time extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      day: 1,
-      month: 1,
-      year: 2017,
-      table: []
     };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange = (event) => {
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-    handleSubmit = (event) => {
-      event.preventDefault();
-
-		fetch("http://localhost:8081/topsongsfrom/" + this.state.month + "_" + this.state.day + "_" + this.state.year,
-		{
-		  method: 'GET' // The type of HTTP request.
-		}).then(response => response.json()).then((data) => {
-      console.log(data.rows)
-      var result = data.rows;
-      console.log(result[0]);
-      let songDivs = result.map((songObj, i) =>
-			<TopSongRow key={i} title={songObj[0]} artists={songObj[1]} streams={songObj[2]}/>
-			  );
-			  this.setState({
-        songs: songDivs
-			  });
-
-      });
-    }
 
   render() {
     return (
@@ -57,52 +22,13 @@ export default class Time extends React.Component {
             <p>Different analyses of top songs from 2017-2018</p>
             <form>
               <Button variant="btn btn-success" href="http://localhost:3000/monthlyartists">Monthly Top Artists</Button>
+              <p></p>
+              <Button variant="btn btn-success" href="http://localhost:3000/topsongs">Top Songs</Button>
+              <p></p>
+              <Button variant="btn btn-success" href="http://localhost:3000/longeststreak">Longest Streak</Button>
             </form>
           </div>
         </div>
-        <form onSubmit = {this.handleSubmit} className="inputForm">
-          <br />
-          <label>
-            Day:
-            <input
-              name="day"
-              type="number"
-              value={this.state.day}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Month:
-            <input
-              name="month"
-              type="number"
-              value={this.state.month}
-              onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Year:
-            <input
-              name="year"
-              type="number"
-              value={this.state.year}
-              onChange={this.handleInputChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        
-        <div className="jumbotron">
-        <div className="movies-container">
-			          <div className="movie">
-                  <div className="header"><strong>title</strong></div>
-                  <div className="header"><strong>artists</strong></div>
-                  <div className="header"><strong>streams</strong></div>
-              </div>
-              <div className="movies-container" id="results">
-              {this.state.songs}
-              </div>
-            </div>
-            </div>
       </div>
 
     );
